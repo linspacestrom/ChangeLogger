39b681ff9d267fc8dd2f397dcc6598033b92dbf3
t.rakhmatullin <figgixs2005@gmail.com>
2025-09-11 01:05:13 +0500

    #4 Добавил структуры под проект

--- internal/domain/entities.go
+++ internal/domain/entities.go
@@ -1,1 +1,19 @@
 package domain
+
+// Project структура под list
+type Project struct {
+	Id    string `json:"uuid"`
+	Title string `json:"title"`
+}
+
+// ProjectDetail структура под detail
+type ProjectDetail struct {
+	Id    string `json:"uuid"`
+	Title string `json:"title"`
+	// TODO нужно будет добавить версионирование
+}
+
+// ProjectCreateOrUpdate структура под create и update
+type ProjectCreateOrUpdate struct {
+	Title string `json:"title"`
+}
