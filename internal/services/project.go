85758452ccc05a2593deae8ad45421a2d3b51ec1
t.rakhmatullin <figgixs2005@gmail.com>
2025-09-11 01:04:30 +0500

    # Добавил сервис проекта

--- internal/services/project.go
+++ internal/services/project.go
@@ -1,1 +1,34 @@
 package services
+
+import (
+	"context"
+	"log"
+
+	"github.com/linspacestrom/ChangeLogger/internal/domain"
+	"github.com/linspacestrom/ChangeLogger/internal/repositories"
+)
+
+type ProjectService struct {
+	repo repositories.ProjectRepository
+}
+
+func NewProjectService(repo repositories.ProjectRepository) *ProjectService {
+	return &ProjectService{repo: repo}
+}
+
+func (s *ProjectService) CreateProject(ctx context.Context, projectCreate domain.ProjectCreateOrUpdate) (domain.Project, error) {
+	project, err := s.repo.Create(ctx, projectCreate)
+	if err != nil {
+		log.Printf("ProjectService failed: %s", err)
+	}
+	return project, err
+
+}
+
+func (s *ProjectService) ListProjects(ctx context.Context) ([]domain.Project, error) {
+	projects, err := s.repo.GetAll(ctx)
+	if err != nil {
+		log.Printf("[ProjectService] failed to list projects: %v\n", err)
+	}
+	return projects, err
+}
