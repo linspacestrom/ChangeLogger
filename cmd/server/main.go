b459ebfbdd6f72518f97227f581982d3ca0735ea
t.rakhmatullin <figgixs2005@gmail.com>
2025-09-10 17:06:00 +0500

    #3 Создал конфиг, его загрузку и подключение к бд

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,54 @@
 package main
+
+import (
+	"ChangeLogger/internal/config"
+	"ChangeLogger/internal/db"
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	log.Println("ChangeLogger started")
+
+	// Обработка контекста с отменой
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Обработка сигналов по graceful shutdown
+	signChan := make(chan os.Signal, 1)
+	signal.Notify(signChan, syscall.SIGINT, syscall.SIGINT)
+	defer close(signChan)
+
+	// загрузка конфига из файла .env
+	cfg, errConfig := config.LoadConfig()
+	if errConfig != nil {
+		log.Printf("Config %s\n", errConfig)
+	}
+
+	log.Println("Config successes loaded")
+
+	// устанавливаем пул соединений с базой данных
+	pgxPool, errPgxPool := db.NewPoolPostgres(ctx, cfg.DbConfig)
+	if errPgxPool != nil {
+		log.Printf("%s\n", errPgxPool)
+		cancel()
+	} else {
+		// закрываем пул соединений с базой данных
+		defer pgxPool.Close()
+	}
+	log.Println("Connection to the database is established")
+
+	// обработчик сигналов
+	select {
+	case <-ctx.Done(): // завершаем работу по контексту
+		log.Println("Context canceling, shutting down")
+	case val := <-signChan: // завершаем работу по graceful shutdown (ctrl+c)
+		log.Printf("\nRecieved signal %s, shutting down", val)
+		cancel()
+	}
+
+	log.Println("ChangeLogger stopped")
+}
--- internal/db/db.go
+++ internal/db/db.go
@@ -1,1 +1,42 @@
 package db
+
+import (
+	"ChangeLogger/internal/config"
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+// buildPostgresDSN функция для создания DatabaseSourceName
+func buildPostgresDSN(cfg config.DbConfig) string {
+	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s", cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.DbName)
+}
+
+// NewPoolPostgres функция для подключения к Postgres
+func NewPoolPostgres(ctx context.Context, cfg config.DbConfig) (*pgxpool.Pool, error) {
+	// создаем DatabaseSourceName
+	dsn := buildPostgresDSN(cfg)
+
+	// создаем пул соединений для Postgres
+	pool, err := pgxpool.New(ctx, dsn)
+	// если не удается создать пул соединений, то подключение к Postgres не получится
+	if err != nil {
+		return nil, fmt.Errorf("create pgx pool: %w", err)
+	}
+
+	// создаем контекст с тайм-аутом для пинга Postgres
+	pgxCtx, cancel := context.WithTimeout(ctx, time.Second*5)
+	defer cancel()
+
+	// делаем пинг для проверки, что удалось подключиться к Postgres
+	if err := pool.Ping(pgxCtx); err != nil {
+		// если не удалось подключиться к Postgres, то закрываем пул соединений
+		pool.Close()
+		return nil, fmt.Errorf("ping to postgres: %w", err)
+	}
+
+	return pool, nil
+
+}
