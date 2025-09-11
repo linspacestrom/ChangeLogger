90a5ce1f0d3a4c66fb25dac9413bcba671978f92
t.rakhmatullin <figgixs2005@gmail.com>
2025-09-11 12:02:11 +0500

    #4 добавил репозитории на деталку и удаление проекта

--- internal/repositories/project.go
+++ internal/repositories/project.go
@@ -10,9 +10,9 @@ import (
 type ProjectRepository interface {
 	Create(ctx context.Context, projectCreate domain.ProjectCreateOrUpdate) (domain.Project, error)
 	GetAll(ctx context.Context) ([]domain.Project, error)
+	GetById(ctx context.Context, id string) (domain.ProjectDetail, error)
+	Delete(ctx context.Context, id string) error
 	//Update(ctx context.Context, uuid string, projectUpdate domain.ProjectCreateOrUpdate) (domain.Project, error)
-	//GetById(ctx context.Context, uuid string) (domain.ProjectDetail, error)
-	//Delete(ctx context.Context, uuid string) error
 }
 
 type PoolProjectRepository struct {
@@ -21,19 +21,6 @@ type PoolProjectRepository struct {
 
 func NewPoolProjectRepository(pool *pgxpool.Pool) *PoolProjectRepository {
 	return &PoolProjectRepository{pool: pool}
-}
-
-func (r *PoolProjectRepository) Create(ctx context.Context, createProject domain.ProjectCreateOrUpdate) (domain.Project, error) {
-	var project domain.Project
-
-	row := r.pool.QueryRow(ctx, `INSERT INTO projects (name) VALUES ($1) RETURNING id, name`, createProject.Title)
-
-	if err := row.Scan(&project.Id, &project.Title); err != nil {
-		return domain.Project{}, err
-	}
-
-	return project, nil
-
 }
 
 func (r *PoolProjectRepository) GetAll(ctx context.Context) ([]domain.Project, error) {
@@ -61,4 +48,37 @@ func (r *PoolProjectRepository) GetAll(ctx context.Context) ([]domain.Project, e
 	}
 
 	return projects, nil
+}
+
+func (r *PoolProjectRepository) Create(ctx context.Context, createProject domain.ProjectCreateOrUpdate) (domain.Project, error) {
+	var project domain.Project
+
+	row := r.pool.QueryRow(ctx, `INSERT INTO projects (name) VALUES ($1) RETURNING id, name`, createProject.Title)
+
+	if err := row.Scan(&project.Id, &project.Title); err != nil {
+		return project, err
+	}
+
+	return project, nil
+
+}
+
+func (r *PoolProjectRepository) GetById(ctx context.Context, id string) (domain.ProjectDetail, error) {
+	var project domain.ProjectDetail
+
+	row := r.pool.QueryRow(ctx, `SELECT * FROM projects WHERE projects.id = $1`, id)
+
+	if err := row.Scan(&project.Id, &project.Title); err != nil {
+		return domain.ProjectDetail{}, err
+	}
+
+	return project, nil
+}
+
+func (r *PoolProjectRepository) Delete(ctx context.Context, id string) error {
+	_, err := r.pool.Exec(ctx, `DELETE FROM projects WHERE projects.id = ($1)`, id)
+	if err != nil {
+		return err
+	}
+	return nil
 }
